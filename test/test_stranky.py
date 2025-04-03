dbca6da1f33e03932bc991412b47008e9e6353a4
GroundPex <teo.plachy@gmail.com>
2025-04-03 13:12:06 +0200

    ne asi, ne

--- test/test_stranky.py
+++ test/test_stranky.py
@@ -1,8 +1,11 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("C:/Users/Teodor/Desktop/PlayWright/playwright-TeodorPlachy/index.html")  # Načte lokální HTML soubor
-    print(page.title())  # Vypíše titulek stránky
-    browser.close()
+def test_page_title():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("C:/Users/Teodor/Desktop/PlayWright/playwright-TeodorPlachy/index.html")  # Načte lokální HTML soubor
+        nadpis_1 = page.locator('h1').first
+        expect(nadpis_1).to_be_visible()
+        browser.close()
+
